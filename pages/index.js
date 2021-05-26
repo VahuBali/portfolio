07346e66429a306e1355733a6ad111d02d74cbf5
VahuBali <67645126+VahuBali@users.noreply.github.com>
2021-05-26 01:34:03 -0400

    Fixing Meta Tags

--- pages/index.js
+++ pages/index.js
@@ -12,10 +12,16 @@ export default function Home() {
       <Head>
         <title>Vasu Bansal</title>
         <link rel="icon" href="/v.svg" />
-        <meta name="description" content="Vasu Bansal's PortFolio, Get any full stack job done in no time with competitive pricing" />
-        <meta name="keywords" content="HTML, CSS, JavaScript, Python, REST API, GraphQL, Node.js, Django, Flask, Next.js, React.js, SwiftUI, Vasu Bansal, Porfolio" />
-        <meta name="image" content="v.svg" />
-        <meta name="author" content="Vasu Bansal" />
+        
+        <meta property="og:title" content="Vasu Bansal" />
+        <meta property="og:type" content="website" />
+        <meta property="og:url" content="https://www.vasubansal.ml/" />
+        <meta property="og:image" content="https://image.flaticon.com/icons/png/512/3522/3522649.png" />
+        <meta property="og:description" content="Vasu Bansal's PortFolio, Get any full stack job done in no time with competitive pricing" />
+        <meta property="og:author" content="Vasu Bansal" />
+        <meta property="og:keywords" content="HTML, CSS, JavaScript, Python, REST API, GraphQL, Node.js, Django, Flask, Next.js, React.js, SwiftUI, Vasu Bansal, Porfolio" />
+        <meta name="theme-color" content="#0000FF" />
+        
       </Head>
 
       <Navbar />
