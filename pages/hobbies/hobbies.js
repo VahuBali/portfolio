a3cb6107c0be1f6b8f1008db0006b9459eeb14a5
VahuBali <67645126+VahuBali@users.noreply.github.com>
2021-05-31 00:22:13 -0400

    Added React exports to blank pages so that they won't throw error#

---
+++ pages/hobbies/hobbies.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import Navbar from '../../components/Home Page/Navbar'
+
+export default function hobbies() {
+    return (
+        <div>
+            <Navbar src="../../v.svg"/>
+
+        </div>
+    )
+}
