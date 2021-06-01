93d2bdae6609fca5ef520338d79eac57b8f12d29
VahuBali <67645126+VahuBali@users.noreply.github.com>
2021-06-01 01:37:06 -0400

    Revert "Added the hobbies page"
    This reverts commit b0f1f88148db39f3adee6382d3960df7ae627549.

--- pages/hobbies/hobbies.js
+++ pages/hobbies/hobbies.js
@@ -1,12 +1,11 @@
 import React from 'react'
 import Navbar from '../../components/Home Page/Navbar'
-import Hobbies from '../../components/Hobbies/Hobbies'
 
 export default function hobbies() {
     return (
         <div>
             <Navbar src="../../v.svg"/>
-            <Hobbies />
+
         </div>
     )
 }
